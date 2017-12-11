fd8d5a538db10b6158be1ca7d39732ed04f868ad
hit1ercopt3r <avatar4ik.cool@gmail.com>
2017-12-11 12:22:39 +0300

    Repo Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daror::Application.config.secret_key_base = 'bdadbf153b6b3f5325a2e2fe677a9a5cf435e9c6a4451fdb02dd9420bb115b4e001a097124206e6533e048b28c05b9ac7d7d24bf43e15b1cae3ca1db41c02b09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Daror::Application.config.session_store :cookie_store, key: '_daror_session'
